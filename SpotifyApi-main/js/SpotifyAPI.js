d1ca52ad1af755edf1917d1510af82d8bb414564
huppanup <94790989+huppanup@users.noreply.github.com>
2025-08-26 17:18:33 -0500

    FIX : Misc changes for verifying auth process

--- SpotifyApi-main/js/SpotifyAPI.js
+++ SpotifyApi-main/js/SpotifyAPI.js
@@ -1,8 +1,9 @@
 
 const clientId = '724a3cf2d2e44418acea58d9eea869af'; // TODO : Replace with global const
+const redirectUri = 'http://localhost:5500/callback';
+
 
 export const _getAuthorization = async (codeChallenge) => {
-    const redirectUri = 'http://localhost:5500/callback'; // TODO : Register redirect URI in Spotify Developer Dashboard
     const scope = 'user-read-private user-read-email';
     const authUrl = new URL("https://accounts.spotify.com/authorize")
 
@@ -37,9 +38,9 @@ export const _getToken = async (code) => {
     }),
   }
 
-  const response = await fetch(url, payload);
+  const res = await fetch(url, payload);
   if (!res.ok) throw new Error("Token exchange failed");
 
-  return await response.json();
+  return await res.json();
 }
 
