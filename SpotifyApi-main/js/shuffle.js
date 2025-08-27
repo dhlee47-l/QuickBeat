39ebbcb6346384837d795b43492915aa1ec8fe54
Donghee Lee <65275537+dhlee47-l@users.noreply.github.com>
2025-08-27 18:35:25 +0900

    Revert "Replaced deprecated playback api"

--- SpotifyApi-main/js/shuffle.js
+++ SpotifyApi-main/js/shuffle.js
@@ -90,6 +90,8 @@ const APIController = (function () {
                 throw new Error('Invalid response structure');
             }
 
+            // preview_url이 아닌 음악 자체를 가져와야 할 듯
+
             return data.items;
         } catch (error) {
             console.error('Error in _getTracks:', error);
@@ -401,12 +403,12 @@ const APPController = (function (UICtrl, APICtrl, FormValidator) {
                 return;
             }
 
-            // TODO : Check if is_playable works as expected
-            const trackData = tracks.filter(e => e.track.name).map(e => ({
+            const trackData = tracks.map(e => ({
                 id: e.track.href,
                 name: e.track.name,
-                artist: e.track.artists[0]?.name,
-                albumImage: e.track.album.images[0]?.url,
+                artist: e.track.artists[0].name,
+                albumImage: e.track.album.images[0].url,
+                previewUrl: e.track.preview_url
             }));
 
             localStorage.setItem('trackData', JSON.stringify(trackData));
